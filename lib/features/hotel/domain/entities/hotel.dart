129eb93c3f96ff46727f3315a08406887e02cae1
Ali Abdelmoaty <68239507+aliabdelmoaty@users.noreply.github.com>
2023-11-10 22:49:10 +0200

    first commit

---
+++ lib/features/hotel/domain/entities/hotel.dart
@@ -0,0 +1,41 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:equatable/equatable.dart';
+
+class Hotel extends Equatable {
+  final String name;
+  final String currency;
+  final String image;
+  final String review;
+  final String address;
+  final double reviewScore;
+  final int starts;
+  final int price;
+  const Hotel({
+    required this.name,
+    required this.currency,
+    required this.image,
+    required this.review,
+    required this.address,
+    required this.reviewScore,
+    required this.starts,
+    required this.price,
+  });
+
+
+
+
+
+  @override
+  List<Object> get props {
+    return [
+      name,
+      currency,
+      image,
+      review,
+      address,
+      reviewScore,
+      starts,
+      price,
+    ];
+  }
+}
---
+++ lib/features/hotel/presentation/cubit/hotel_state.dart
@@ -0,0 +1,10 @@
+part of 'hotel_cubit.dart';
+
+abstract class HotelState extends Equatable {
+  const HotelState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class HotelInitial extends HotelState {}
